5a829f7114ecf74dec170801b4e0f3b9bebfdd91
jayannah <jayannah@gmail.com>
2017-06-15 09:59:53 -0400

    Create install_java.sh

---
+++ General_Installations/install_java.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Download jdk
+
+#Installing java
+ver=1.8.0_73
+echo Installing Java ${ver}
+tar -xvzf jdk-8u73-linux-x64.tar.gz
+mkdir /usr/lib/jvm
+mkdir /usr/lib/jvm/oracle_jdk8
+mv jdk${ver} /usr/lib/jvm/oracle_jdk8
+update-alternatives --install /usr/bin/java java /usr/lib/jvm/oracle_jdk8/jdk${ver}/jre/bin/java 2000
+update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/oracle_jdk8/jdk${ver}/bin/javac 2000
+update-alternatives --config java
+update-alternatives --config javac
+echo "export J2SDKDIR=/usr/lib/jvm/oracle_jdk8/jdk${ver}" >> /etc/profile.d/oraclejdk.sh
+echo "export J2REDIR=/usr/lib/jvm/oracle_jdk8/jdk${ver}/jre" >> /etc/profile.d/oraclejdk.sh
+echo "export PATH=$PATH:/usr/lib/jvm/oracle_jdk8/jdk${ver}/bin:/usr/lib/jvm/oracle_jdk8/jdk${ver}/db/bin:/usr/lib/jvm/oracle_jdk8/jdk${ver}/jre/bin" >> /etc/profile.d/oraclejdk.sh
+echo "export JAVA_HOME=/usr/lib/jvm/oracle_jdk8/jdk${ver}" >> /etc/profile.d/oraclejdk.sh
+echo "export DERBY_HOME=/usr/lib/jvm/oracle_jdk8/jdk${ver}/db" >> /etc/profile.d/oraclejdk.sh
+sh /etc/profile.d/oraclejdk.sh
+chmod -R 755 /usr/lib/jvm
+
+#Remove file
